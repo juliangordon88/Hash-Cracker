72c832202f670bcfa8b2d33a7270e0e101913241
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 12:25:27 +0100

    agregamos comentarios

--- hash-cracker.py
+++ hash-cracker.py
@@ -1,19 +1,25 @@
 import hashlib
 
+
+#pedimos el hash
 hash_file = input("Enter the hash: ")
 
+#pedimos la ruta al diccionario
 dic_file = input("Enter the dictionary file path: ")
 
+#leemos el diccionario
 with open(dic_file, 'r') as file:
 
     diccionario = [line.strip() for line in file]
 
     for password in diccionario:
-
+        #aqui debemos elegir el tipo de hash que queremos
         calculated_hash = hashlib.md5(password.encode()).hexdigest()
 
+        #si lo encontramos en el diccionario
         if calculated_hash == hash_file:
             print("The password is: ", password)
             break
+        #si no lo encontramos
         else:
             print("The password is not in the dictionary ")
